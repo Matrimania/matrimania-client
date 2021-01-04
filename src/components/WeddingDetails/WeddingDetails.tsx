4f7378e57ade809ff7de5895a96f99706eda7eba
Kristi Miller <kristi.ann.miller@gmail.com>
2021-01-04 13:48:15 -0700

    Add WeddingDetails component

---
+++ src/components/WeddingDetails/WeddingDetails.tsx
@@ -0,0 +1,11 @@
+import './WeddingDetails.css';
+
+
+function WeddingDetails() {
+    return (
+        <section className="detailsWrapper">
+            <div>WeddingDetails</div>
+        </section>
+    )
+}
+export default WeddingDetails;
