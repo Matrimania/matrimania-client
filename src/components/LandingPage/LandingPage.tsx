fd31cb9dd7fba658b839f471dcf9bc1a1660859f
Kristi Miller <kristi.ann.miller@gmail.com>
2021-01-04 13:47:40 -0700

    Add LandingPage component

---
+++ src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,11 @@
+import './LandingPage.css';
+
+
+function LandingPage() {
+    return (
+        <section className="landingWrapper">
+            <div>LandingPage</div>
+        </section>
+    )
+}
+export default LandingPage;
