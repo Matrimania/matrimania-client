4af800674bb6a052e37f326a80116ad875b42989
Kristi Miller <kristi.ann.miller@gmail.com>
2021-01-04 15:24:19 -0700

    Add WeddingCard component

---
+++ src/components/WeddingCard/WeddingCard.tsx
@@ -0,0 +1,11 @@
+import './WeddingCard.css';
+
+
+function WeddingCard() {
+    return (
+        <section className="cardWrapper">
+            <div>WeddingCard</div>
+        </section>
+    )
+}
+export default WeddingCard;
