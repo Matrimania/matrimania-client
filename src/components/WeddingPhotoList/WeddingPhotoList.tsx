616e0f02f823a84608eaff34ba867160efd21e93
Kristi Miller <kristi.ann.miller@gmail.com>
2021-01-04 13:48:29 -0700

    Add WeddingPhotoList component

---
+++ src/components/WeddingPhotoList/WeddingPhotoList.tsx
@@ -0,0 +1,11 @@
+import './WeddingPhotoList.css';
+
+
+function WeddingPhotoList() {
+    return (
+        <section className="detailsWrapper">
+            <div>WeddingPhotoList</div>
+        </section>
+    )
+}
+export default WeddingPhotoList;
