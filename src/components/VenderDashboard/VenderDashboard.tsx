fbae59c8951d72e69908ac803ba16e06320714bc
Kristi Miller <kristi.ann.miller@gmail.com>
2021-01-04 13:48:00 -0700

    Add VenderDashboard component

---
+++ src/components/VenderDashboard/VenderDashboard.tsx
@@ -0,0 +1,21 @@
+import './VenderDashboard.css';
+
+
+function VenderDashboard() {
+    return (
+        <section className="dashboardWrapper">
+            <section>Filter By:
+                <select>
+                    <option value="0">All</option>
+                    <option value="1">Upcoming</option>
+                    <option value="2">Past</option>
+                    <option value="3">Today</option>
+                </select>
+            </section>
+            <section className="weddingCardWrap">
+            </section>
+            <button className="addWeddingButton">+</button>
+        </section>
+    )
+}
+export default VenderDashboard;
