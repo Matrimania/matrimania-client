ce72f88548a07228e28863e13fab00703dfc008c
Kristi Miller <kristi.ann.miller@gmail.com>
2021-01-04 15:24:47 -0700

    Adjust spacing

--- src/components/VenderDashboard/VenderDashboard.tsx
+++ src/components/VenderDashboard/VenderDashboard.tsx
@@ -1,20 +1,22 @@
 import './VenderDashboard.css';
+import WeddingCard from '../WeddingCard/WeddingCard'
+
 
 
 function VenderDashboard() {
     return (
         <section className="dashboardWrapper">
-            <section>Filter By:
-                <select>
-                    <option value="0">All</option>
-                    <option value="1">Upcoming</option>
-                    <option value="2">Past</option>
-                    <option value="3">Today</option>
-                </select>
-            </section>
-            <section className="weddingCardWrap">
-            </section>
-            <button className="addWeddingButton">+</button>
+          <section>Filter By:
+            <select>
+                <option value="0">All</option>
+                <option value="1">Upcoming</option>
+                <option value="2">Past</option>
+                <option value="3">Today</option>
+            </select>
+          </section>
+          <section className="weddingCardWrap">
+          </section>
+          <button className="addWeddingButton">+</button>
         </section>
     )
 }
