b853d49572fa9a38951cecb814403ca41b939c86
Kristi Miller <kristi.ann.miller@gmail.com>
2021-01-05 13:53:49 -0700

    Add styles

--- src/components/VenderDashboard/VenderDashboard.tsx
+++ src/components/VenderDashboard/VenderDashboard.tsx
@@ -16,18 +16,24 @@ function VenderDashboard() {
   })
     return (
         <section className="dashboardWrapper">
-          <section>Filter By:
-            <select>
+          <section className="optionsWrap">
+          <div className="button" id="addWeddingButton">
+            <div id="translate"></div>
+            <a>Add A Wedding</a>
+          </div>
+            <section className="filterWrap">
+            <label className="label">Filter By :</label>
+            <select className="button">
                 <option value="0">All</option>
                 <option value="1">Upcoming</option>
                 <option value="2">Past</option>
                 <option value="3">Today</option>
             </select>
+            </section>
           </section>
           <section className="weddingCardWrap">
             {weddingCards}
           </section>
-          <button className="addWeddingButton">+</button>
         </section>
     )
 }
