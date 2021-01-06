6dfe2251dc7d008c8954884e9b8d03a5735431ae
Hanna Kim <hannakim91@gmail.com>
2021-01-05 19:05:51 -0700

    Create form elements

--- src/components/GuestListForm/GuestListForm.tsx
+++ src/components/GuestListForm/GuestListForm.tsx
@@ -1,11 +1,30 @@
 import React, { useState } from 'react';
 
 const GuestListForm: React.FC = () => {
-
+  
 	return (
-		<section className="formWrapper">
-			<div>GuestListForm</div>
-		</section>
+
+    <form className="formWrapper">
+    <h1>Add Guest</h1>
+      <input
+        type='text'
+        placeholder='Guest Name'
+        name='guestName'
+        value={guestName}
+        onChange={event => setGuestName(event.target.value)}
+      />
+
+      <input
+        type='text'
+        placeholder='Phone Number'
+        name='phoneNumber'
+        value={phoneNumber}
+        onChange={event => setPhoneNumber(event.target.value)}
+      />
+
+      <button onClick={event => submitIdea(event)}> SUBMIT
+      </button>
+    </form>
 	)
 }
 
