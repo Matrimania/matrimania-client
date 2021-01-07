3141d5c1f18b811dd9d6e638669390abd7071e5f
Eric Berglund <ericberglund117@gmail.com>
2021-01-06 19:47:46 -0700

    Create RequestDetailsForm component to import into weddingDetails

---
+++ src/components/RequestDetailsForm/RequestDetailsForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './RequestDetailsForm.css'
+
+const RequestDetailsForm: React.FC = () => {
+    //need link to google spreadsheet and customer email
+
+		return (
+			
+		)
+}
