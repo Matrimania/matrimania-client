a86448b949f23a8e85ab6276d22aee2ed9a167a7
Hanna Kim <hannakim91@gmail.com>
2021-01-07 19:44:59 -0700

    Add describe/it blocks

--- src/__tests__/WeddingDetails.tsx
+++ src/__tests__/WeddingDetails.tsx
@@ -3,3 +3,9 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
 import WeddingDetails from '../components/WeddingDetails/WeddingDetails';
+
+describe('WeddingDetails', () => {
+  it('renders WeddingDetails elements', () => {
+  });
+
+});
