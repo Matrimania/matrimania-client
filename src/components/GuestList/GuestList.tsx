056274ba13bcba44509bcee19f6120902cd9a4dd
Eric Berglund <ericberglund117@gmail.com>
2021-01-07 18:41:27 -0700

    Add dashes to phoneNumber input. bug missing last two numbers

--- src/components/GuestList/GuestList.tsx
+++ src/components/GuestList/GuestList.tsx
@@ -9,10 +9,11 @@ import { StyledButton, StyledCard } from '../App/styledComponents.styles'
 type NewGuest = {
   id: number,
   guestName: string,
-  phoneNumber: number
+  phoneNumber: any;
 }
 
 const GuestList: React.FC = () => {
+<<<<<<< HEAD
 
 <<<<<<< HEAD
   const [guestName, setGuestName] = useState('')
@@ -24,38 +25,52 @@ const GuestList: React.FC = () => {
       filter.push(i + keypadZero)
       filter.push(i + numpadZero)
     }
+=======
+  
+  const [guestName, setGuestName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [guests, setGuests] = useState<NewGuest[]>([]);
+  let filter: any = [];
+  const keypadZero: number = 48;
+  const numpadZero: number = 96;
 
-    filter.push(8); 
-    filter.push(9);
-    filter.push(46);
-    filter.push(37);
-    filter.push(39);
-  }
+  // const organizePhoneInput = (event: any) => {
+  //   for(let i = 0; i <= 9; i++) {
+  //     filter.push(i + keypadZero)
+  //     filter.push(i + numpadZero)
+  //   }
+>>>>>>> 170ecbde... Add dashes to phoneNumber input. bug missing last two numbers
 
-  const onKeyDown = (event) => {
-    if(filter.indexOf(event.keyCode) < 0) {
-      event.preventDefault()
-      return false
-    }
-  }
+  //   filter.push(8); 
+  //   filter.push(9);
+  //   filter.push(46);
+  //   filter.push(37);
+  //   filter.push(39);
+  //   onKeyDown(event)
+  // }
+
+  // const onKeyDown = (event: any) => {
+  //   console.log('down')
+  //   if(filter.indexOf(event.keyCode) < 0) {
+  //     return false
+  //   }
+  //   onKeyUp()
+  // }
 
   const formatPhoneText = (value: string) => {
-    value = this.replaceAll(value.trim(), "-", "");
+    value = value.trim().replaceAll( "-", "");
     if (value.length > 3 && value.length <= 6) {
       value = value.slice(0,3) + "-" + value.slice(3);
     } else if(value.length > 6) {
       value = value.slice(0,3) + "-" + value.slice(3,6) + "-" + value.slice(6);
     }
-
-    return value
+    setPhoneNumber(value)
   }
 
-  const validatePhone = (p: string) => {
-    let phoneRE = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
-    let digits = p.replace(/\D/g, "");
-    return phoneRE.test(digits)
-  }
+  //   return value
+  // }
 
+<<<<<<< HEAD
   const onKeyUp = (event) => {
     let input = event.target;
     let formatted = formatPhoneText(input.value)
@@ -68,8 +83,29 @@ const GuestList: React.FC = () => {
   }
   
 >>>>>>> 542d4a89... Create validatePhone function
+=======
+  // const validatePhone = (p: string) => {
+  //   let phoneRE = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+  //   let digits = p.replace(/\D/g, "");
+  //   return phoneRE.test(digits)
+  // }
+>>>>>>> 170ecbde... Add dashes to phoneNumber input. bug missing last two numbers
 
+  // const onKeyUp = () => {
+  //   console.log("up")
+  //   let input = phoneNumber.toString();
+  //   let formatted = formatPhoneText(input)
+  //   let isError = (validatePhone(formatted) || formatted.length === 0)
+  //   let color = (isError) ? "gray" : "red";
+  //   let borderWidth = (isError) ? "1px" : "3px";
+  //   input.style.borderColor = color;
+  //   input.style.borderWidth = borderWidth;
+  //   setPhoneNumber(formatted);
+  // }
+
+  
   const submitGuest = (event: React.FormEvent) => {
+    console.log(phoneNumber)
     event.preventDefault();
     const newGuest: NewGuest = {
       id: Date.now(),
@@ -83,7 +119,7 @@ const GuestList: React.FC = () => {
 
   const clearInputs = () => {
     setGuestName('')
-    setPhoneNumber(0)
+    setPhoneNumber('')
   }
 
   const deleteGuest = (id: number) => {
@@ -106,12 +142,17 @@ const GuestList: React.FC = () => {
           onChange={event => setGuestName(event.target.value)}
         />
         <input
+<<<<<<< HEAD
           type='tel'
           placeholder='Phone Number'
+=======
+          type='text'
+          placeholder='Phone Number (XXX-XXX-XXXX)'
+>>>>>>> 170ecbde... Add dashes to phoneNumber input. bug missing last two numbers
           name='phoneNumber'
           maxLength={10}
-          value={phoneNumber !== 0 ? phoneNumber : ''}
-          onChange={event => setPhoneNumber(parseInt(event.target.value))}
+          value={phoneNumber}
+          onChange={event => formatPhoneText(event.target.value)}
         />
         <StyledButton>
           <div id="translate"></div>
