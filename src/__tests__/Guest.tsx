4d98e7837f721a08323c5297aa403701bc8030b4
Hanna Kim <hannakim91@gmail.com>
2021-01-07 19:51:25 -0700

    Unit test Guest card renders

--- src/__tests__/Guest.tsx
+++ src/__tests__/Guest.tsx
@@ -5,7 +5,18 @@ import { MemoryRouter } from 'react-router-dom';
 import Guest from '../components/Guest/Guest';
 
 describe('Guest', () => {
-  it('renders Guest elements', () => {
+  it('renders Guest card', () => {
+    render(
+      <Guest
+        id={1}
+        key={1}
+        guestName={"Bob Loblaw"}
+        phoneNumber={1234567890}
+      />
+    );
+    expect(screen.getByText("Bob Loblaw")).toBeInTheDocument();
+    expect(screen.getByText("1234567890")).toBeInTheDocument();
+    expect(screen.getByRole("button",{name:"Delete 🗑"})).toBeInTheDocument();
   });
 
 });
