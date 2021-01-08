442856649a8ac90dceb014e9a497f1217d9d96d7
Hanna Kim <hannakim91@gmail.com>
2021-01-07 19:46:06 -0700

    Set up test file for Guest

---
+++ src/__tests__/Guest.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import Guest from '../components/Guest/Guest';
+
+describe('Guest', () => {
+  it('renders Guest elements', () => {
+  });
+
+});
