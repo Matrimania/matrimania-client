76415b5e040edb6bf9df15e5c2a5fcc912fd6ed7
Kristi Miller <kristi.ann.miller@gmail.com>
2021-01-10 15:30:52 -0700

    Add functionality for weddings to sort according to date on vendor dashboard

--- src/components/WeddingCard/WeddingCard.tsx
+++ src/components/WeddingCard/WeddingCard.tsx
@@ -2,13 +2,15 @@ import './WeddingCard.css';
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { StyledCard } from '../App/styledComponents.styles'
+import dayjs from 'dayjs';
+
 
 
 type Wedding = {
   id: number;
   name: string;
   image: string;
-  date: string;
+  date: any;
 }
 
 const WeddingCard: React.FC<Wedding> = ({
@@ -20,7 +22,7 @@ const WeddingCard: React.FC<Wedding> = ({
       <StyledCard contents="wedding">
         <Link to={`/wedding/${id}`}>
             <h1 className="weddingTitle">{name} Wedding</h1>
-            <h2 className="weddingDate">{date}</h2>
+            <h2 className="weddingDate">{dayjs(date).format("MM/DD/YYYY")}</h2>
             <section className="couplePhotoWrap">
               <img className="couplePhoto" src={image} alt={`Photo of couple: ${name}`}/>
             </section>
