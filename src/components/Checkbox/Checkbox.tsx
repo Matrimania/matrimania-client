1163c701b79bb6d6b2aff9498ad915404341572b
Hanna Kim <hannakim91@gmail.com>
2021-01-10 16:23:59 -0700

    Create basic Checkbox component

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {
+
+}
+
+const CheckBox: React.FC<Props> = () => {
+  return (
+    <div>
+      Checkbox
+    </div>
+  )
+}
+
+
+export default CheckBox
--- src/components/PhotoListForm/PhotoListForm.tsx
+++ src/components/PhotoListForm/PhotoListForm.tsx
@@ -1,6 +1,7 @@
 import Guest from '../Guest/Guest';
 import React, { useState, useEffect } from 'react';
 import Photo from '../Photo/Photo';
+import Checkbox from '../Checkbox/Checkbox';
 
 
 type NewPhoto = {
