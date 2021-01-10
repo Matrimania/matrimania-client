f617df59d4b7af756a1be56b9aa823d9556f7a44
Hanna Kim <hannakim91@gmail.com>
2021-01-10 11:40:25 -0700

    Add types for NewPhoto and WeddingData

---
+++ src/components/PhotoListForm/PhotoListForm.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Photo from '../Photo/Photo';
+
+type NewPhoto = {
+  id: number;
+  photoNumber: string;
+  guests: string[];
+  description: string;
+}
+
+type WeddingData = {
+	changeView: any;
+}
