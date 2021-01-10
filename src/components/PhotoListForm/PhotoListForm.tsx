65229b78be2cf4658c3f5410aa2c7f904e16db6f
Hanna Kim <hannakim91@gmail.com>
2021-01-10 12:22:43 -0700

    Add form instructions

--- src/components/PhotoListForm/PhotoListForm.tsx
+++ src/components/PhotoListForm/PhotoListForm.tsx
@@ -14,10 +14,18 @@ type WeddingData = {
 }
 
 const PhotoListForm: React.FC<WeddingData> = ({changeView}) => {
+  const [description, setDescription] = useState('');
+  const [guestsInPhoto, setGuestsInPhoto] = useState([]);
+
   return (
-    <>
-    PhotoListForm
-    </>
+    <article>
+      <form className="formWrapper">
+        <article className="instructionWrap">
+          <h1 className="weddingTitle"style={{fontSize: '3vw'}}>Let's start with your photo list</h1>
+          <h2 className="weddingDate" style={{fontSize: '1.5vw', padding: '2% 10%', textAlign: 'left'}}>To add a photo: <br></br><br></br>1. Add a guest <br></br>2. Add an optional description<br></br>3. Click Submit Photo button <br></br>Tip: Don't forget to include yourselves!</h2>
+        </article>
+      </form>
+    </ article>
 	)
 }
 
