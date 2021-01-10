b6af625effd6d1c2c7293d5f6b171f1398b18723
Hanna Kim <hannakim91@gmail.com>
2021-01-10 12:44:11 -0700

    Create input for description

--- src/components/PhotoListForm/PhotoListForm.tsx
+++ src/components/PhotoListForm/PhotoListForm.tsx
@@ -24,6 +24,14 @@ const PhotoListForm: React.FC<WeddingData> = ({changeView}) => {
           <h1 className="weddingTitle"style={{fontSize: '3vw'}}>Let's start with your photo list</h1>
           <h2 className="weddingDate" style={{fontSize: '1.5vw', padding: '2% 10%', textAlign: 'left'}}>To add a photo: <br></br><br></br>1. Add a guest <br></br>2. Add an optional description<br></br>3. Click Submit Photo button <br></br>Tip: Don't forget to include yourselves!</h2>
         </article>
+        <input
+          type='text'
+          placeholder='Description'
+          name='description'
+          value={description}
+          onChange={event => setDescription(event.target.value)}
+        />
+
       </form>
     </ article>
 	)
