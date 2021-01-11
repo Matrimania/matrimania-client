367f79d3e1e3922fb37e57464f776705fd4c652f
Eric Berglund <ericberglund117@gmail.com>
2021-01-11 14:48:50 -0700

    Add user should be able to delete a guest it block to Guest.test

--- src/__tests__/Guest.test.tsx
+++ src/__tests__/Guest.test.tsx
@@ -22,8 +22,26 @@ describe('Guest', () => {
     expect(screen.getByText("Bob Loblaw")).toBeInTheDocument();
     expect(screen.getByText("1234567890")).toBeInTheDocument();
     expect(deleteButton).toBeInTheDocument();
+  });
+
+  it('should allow the user to delete a guest', () => {
+    const deleteGuest= jest.fn()
+
+    render(
+      <Guest
+        id={1}
+        key={1}
+        guestName={"Bob Loblaw"}
+        phoneNumber={'1234567890'}
+        deleteGuest={deleteGuest}
+      />
+    );
+    const deleteButton = screen.getByRole("button",{name:"X"})
+    expect(screen.getByText("Bob Loblaw")).toBeInTheDocument();
+    expect(screen.getByText("1234567890")).toBeInTheDocument();
+    expect(deleteButton).toBeInTheDocument();
     userEvent.click(deleteButton)
     expect(deleteGuest).toHaveBeenCalledTimes(1)
-  });
+  })
 
 });
