bfb405db88f9f482ab6c6656dd724f69f48b0ca9
Eric Berglund <ericberglund117@gmail.com>
2021-01-11 16:11:54 -0700

    Pass renders photo page on Photo.test

--- src/__tests__/Photo.test.tsx
+++ src/__tests__/Photo.test.tsx
@@ -4,22 +4,27 @@ import { MemoryRouter } from 'react-router-dom';
 
 import Photo from '../components/Photo/Photo';
 
-describe('WeddingCard', () => {
-  it('renders WeddingCard card', () => {
+describe('Photo', () => {
+  it('renders Photo page', () => {
     render(
       <MemoryRouter>
         <Photo
         id={1}
         photoNumber={1}
-        guests={["Gary", "Jim", "Mary", "Bob"]}
+        guests={[
+            {isChecked: true, name: 'Bacob', phone: 5555551234, photos: [1, 2, 3, 4, 6]},
+            {isChecked: true, name: 'Bohn', phone: 1111151234, photos: [1, 2, 3, 4, 6]},
+            {isChecked: true, name: 'Bamy', phone: 2222221234, photos: [1, 2, 3, 4, 6]}
+        ]}
         description={"Rebellious Bunch"}
       />
-      </MemoryRouter>
-      
+      </MemoryRouter> 
     );
-    expect(screen.getByText("Jim & Pam Wedding")).toBeInTheDocument();
-    expect(screen.getByText("10/08/2009")).toBeInTheDocument();
-    expect(screen.getByAltText("Photo of couple: Jim & Pam")).toBeInTheDocument();
+    
+    expect(screen.getByText('Photo 1')).toBeInTheDocument();
+    expect(screen.getByText('Guests: BacobBohnBamy')).toBeInTheDocument();
+    expect(screen.getByText('Description: Rebellious Bunch')).toBeInTheDocument();
+    
   });
 
 });
