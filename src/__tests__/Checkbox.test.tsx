47a067a7b1ac1c09c7fdc7563b1c154ce06fb9ac
Eric Berglund <ericberglund117@gmail.com>
2021-01-11 15:36:15 -0700

    Add user toggleCheckMark test to Checkbox.test

--- src/__tests__/Checkbox.test.tsx
+++ src/__tests__/Checkbox.test.tsx
@@ -18,4 +18,23 @@ describe('Checkbox', () => {
 		expect(checkbox).toBeInTheDocument();
 		expect(screen.getByText('Mayer Hawthorne')).toBeInTheDocument();
 	});
+
+	it('should put a check next to a guest when a user clicks a checkbox', () => {
+		const mockToggleCheckMark = jest.fn()
+		const isChecked = false
+		render(
+			<Checkbox
+				name={'Mayer Hawthorne'}
+				isChecked={isChecked}
+				toggleCheckMark={mockToggleCheckMark}
+			/>
+		);
+
+		const checkbox = screen.getByRole('checkbox')
+		expect(checkbox).toBeInTheDocument();
+		expect(screen.getByText('Mayer Hawthorne')).toBeInTheDocument();
+		userEvent.click(checkbox)
+		expect(mockToggleCheckMark).toHaveBeenCalled();
+		
+	});
 });
