9b24e1923cc122a0e17844c03b9e481781fb7088
Kristi Miller <kristi.ann.miller@gmail.com>
2021-01-10 21:08:07 -0700

    Add PhotoShootView component

---
+++ src/components/PhotoShootView/PhotoShootView.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PhotoShootView.css'
+
+
+const PhotoShootView: React.FC = () => {
+  return (
+    <div>
+      PhotoShootView
+    </div>
+  )
+}
+
+export default PhotoShootView;
