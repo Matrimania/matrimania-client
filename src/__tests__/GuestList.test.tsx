ddf0fc1dd88f531a47bea67e257adb2ef400c6ca
Eric Berglund <ericberglund117@gmail.com>
2021-01-11 11:19:32 -0700

    Create GuestList testing file

---
+++ src/__tests__/GuestList.test.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { individualWedding } from '../weddingData'
+import GuestList from '../components/GuestList/GuestList';
