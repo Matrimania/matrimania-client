199e4106672b0048beac7b6f27315fe00c5a3a62
Eric Berglund <ericberglund117@gmail.com>
2021-01-10 17:26:48 -0700

    Create default WeddingDetails it block on WeddingDetails.test

--- src/__tests__/WeddingDetails.test.tsx
+++ src/__tests__/WeddingDetails.test.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-
+import { individualWedding } from '../weddingData'
 import WeddingDetails from '../components/WeddingDetails/WeddingDetails';
 
 describe('WeddingDetails', () => {
-  it('renders WeddingDetails elements', () => {
+  it('renders default WeddingDetails elements', () => {
+    render(
+    <MemoryRouter>
+      <WeddingDetails
+        weddingId={individualWedding.weddingId}
+        name={individualWedding.name}
+        image={individualWedding.image}
+        date={individualWedding.date}
+        email={individualWedding.email}
+        familyPhotoList={individualWedding.familyPhotoList}
+        photoList={individualWedding.photoList}
+        />
+    </MemoryRouter>
+    );
+    
   });
 
 });
