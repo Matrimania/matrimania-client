9323544e6b373cacd8fb4aca2426338558310158
Eric Berglund <ericberglund117@gmail.com>
2021-01-11 11:30:53 -0700

    Test user input form is displayed

--- src/__tests__/GuestList.test.tsx
+++ src/__tests__/GuestList.test.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { individualWedding } from '../weddingData'
+import userEvent from '@testing-library/user-event'
+
 import GuestList from '../components/GuestList/GuestList';
+
 
 describe("GuestList",() => {
 	it('should render a guestlist page', () => {
@@ -18,5 +21,11 @@ describe("GuestList",() => {
 		const guestListInstructions = screen.getByRole('heading', {  name: `For each person included in your family photos, please include: 1. Their first and last name 2. A mobile phone number that accepts text messages Don't forget yourselves!`})
 		expect(guestListTitle).toBeInTheDocument();
 		expect(guestListInstructions).toBeInTheDocument();
-	})
+		expect(screen.getByPlaceholderText('Guest Name')).toBeInTheDocument();
+		expect(screen.getByPlaceholderText('Phone (XXX-XXX-XXXX)')).toBeInTheDocument();
+	});
 })
+
+// test display is correct
+// test format phone number function 
+// userEvent.type 
