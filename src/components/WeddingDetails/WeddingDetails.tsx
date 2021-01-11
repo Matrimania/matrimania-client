1e3879c07eb8b0df5d7e0c4ee2bd233d55a9148b
Hanna Kim <hannakim91@gmail.com>
2021-01-11 13:27:52 -0700

    Remove unused code

--- src/components/WeddingDetails/WeddingDetails.tsx
+++ src/components/WeddingDetails/WeddingDetails.tsx
@@ -116,10 +116,6 @@ const WeddingDetails: React.FC<IndividualWedding> = ({
 						<a className="link" id="addListButton">Add Photo List</a>
 					}
 				</StyledButton>
-				{/* <PhotoListForm 
-					guests={familyPhotoList}
-					changeView={determineCurrentState}
-				/> */}
 				{photoList.length > 0 &&
 					<WeddingPhotoList
 						name={individualWedding.name}
