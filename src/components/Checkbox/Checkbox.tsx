172f2afda3f02c15625b2a56bd93993328344e78
Hanna Kim <hannakim91@gmail.com>
2021-01-11 17:40:15 -0700

    Align Checkbox elements

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -10,9 +10,12 @@ type Props = {
 
 const Checkbox: React.FC<Props> = (props) => {
   return (
-    <div className="checkbox">
-      <label>{props.name}</label>
+    <div className="checkboxContainer">
+      <label
+        className="checkboxLabel"
+      >{props.name}</label>
       <input 
+        className="checkbox"
         type="checkbox" 
         name={props.name}
         checked={props.isChecked}
