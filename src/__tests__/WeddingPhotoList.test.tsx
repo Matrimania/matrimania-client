ac0efcbc4041fbefe654f6ce28d6bf99f6b95c80
Eric Berglund <ericberglund117@gmail.com>
2021-01-11 20:00:06 -0700

    Pass render WeddingPhotoList page on WeddingPhotoList.test

--- src/__tests__/WeddingPhotoList.test.tsx
+++ src/__tests__/WeddingPhotoList.test.tsx
@@ -7,6 +7,18 @@ import WeddingPhotoList from '../components/WeddingPhotoList/WeddingPhotoList';
 
 describe('WeddingPhotoList', () => {
 	it('should render the WeddingPhotoList page', () => {
-		
-	})
-})
+		const photoList = [{photoId: 1, guests:['Clarence', 'Schmidt', 'Cher'], description: 'Rowdy'}]
+		render(
+			<MemoryRouter>
+				<WeddingPhotoList 
+					name='Jumbalaya Smithers'
+					weddingId={1}
+					photoList={photoList}
+				/>
+			</MemoryRouter>
+		)
+		expect(screen.getByText('Photo: 1')).toBeInTheDocument();
+		expect(screen.getByText('Guests: ClarenceSchmidtCher')).toBeInTheDocument();
+		expect(screen.getByText('Description: Rowdy')).toBeInTheDocument();
+	});
+});
