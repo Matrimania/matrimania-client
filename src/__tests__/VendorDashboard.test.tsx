892d49551ed45a1fdf9f6b7c4f804b2ffdb3d52f
Eric Berglund <ericberglund117@gmail.com>
2021-01-11 19:27:36 -0700

    Pass user can filter by on VendorDashboard.test

--- src/__tests__/VendorDashboard.test.tsx
+++ src/__tests__/VendorDashboard.test.tsx
@@ -17,5 +17,35 @@ describe('VendorDashboard', () => {
 		expect(screen.getByText('Filter By :')).toBeInTheDocument();
 		expect(screen.getByRole('combobox')).toBeInTheDocument();
 		expect(screen.getByText('All')).toBeInTheDocument();
-	})
+	});
+
+	it('should allow the user to select a filter by option', () => {
+		render(
+			<MemoryRouter>
+				<VendorDashboard />
+			</MemoryRouter>
+		)
+		const addWeddingButton = screen.getByRole('link', { name: /add a wedding/i })
+		expect(addWeddingButton).toBeInTheDocument();
+		expect(screen.getByText('Filter By :')).toBeInTheDocument();
+		expect(screen.getByRole('combobox')).toBeInTheDocument();
+
+		const filterAll = screen.getByText('All');
+		const filterUpcoming = screen.getByText('Upcoming')
+		const filterPast = screen.getByText('Past')
+		const filterToday = screen.getByText('Today')
+		
+		userEvent.click(filterAll)
+		expect(filterAll).toBeInTheDocument();
+
+		userEvent.click(filterUpcoming)
+		expect(filterUpcoming).toBeInTheDocument();
+
+		userEvent.click(filterPast)
+		expect(filterPast).toBeInTheDocument();
+
+		userEvent.click(filterToday)
+		expect(filterToday).toBeInTheDocument();
+	});
+
 })
