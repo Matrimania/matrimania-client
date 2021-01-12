8dd4081a737f53b939205035f0e3f02d73e7b837
Eric Berglund <ericberglund117@gmail.com>
2021-01-11 18:18:24 -0700

    Test user can check guests in PhotoListForm.test

--- src/__tests__/PhotoListForm.test.tsx
+++ src/__tests__/PhotoListForm.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event'
 
 import PhotoListForm from '../components/PhotoListForm/PhotoListForm';
 
@@ -49,5 +50,40 @@ describe('PhotoListForm', () => {
 		expect(screen.getByPlaceholderText('Description')).toBeInTheDocument();
 		expect(screen.getByRole('button', {name: 'Submit'})).toBeInTheDocument();
 		expect(screen.getByText('Your Photos')).toBeInTheDocument()
+	});
+
+	it('should allow the user to check guests', () => {
+		const mockChangeView = jest.fn()
+	
+    render(
+      <MemoryRouter>
+        <PhotoListForm
+        guests={[
+            {isChecked: false, name: 'Bacob', phone: 5555551234, photos: [1, 2, 3, 4, 6]},
+            {isChecked: false, name: 'Bohn', phone: 1111151234, photos: [1, 2, 3, 4, 6]},
+            {isChecked: false, name: 'Bamy', phone: 2222221234, photos: [1, 2, 3, 4, 6]}
+        ]}
+        changeView={mockChangeView}
+      />
+      </MemoryRouter> 
+    );
+				
+		const checkbox = screen.getAllByRole('checkbox')
+
+		expect(screen.getByRole('heading', {  name: /let's start with your photo list/i})).toBeInTheDocument();
+		expect(screen.getByRole('heading', { name: "To add a photo: 1. Add a description (optional) 2. Pick guests to include in the photo 3. Click Submit button Tip: Don't forget to include yourselves!"})).toBeInTheDocument();
+		expect(screen.getByPlaceholderText('Description')).toBeInTheDocument();
+		expect(screen.getByRole('button', {name: 'Submit'})).toBeInTheDocument();
+		expect(screen.getByText('Your Photos')).toBeInTheDocument()
+		expect(screen.getByText("Bacob")).toBeInTheDocument();
+		expect(screen.getByText("Bohn")).toBeInTheDocument();
+		expect(screen.getByText("Bamy")).toBeInTheDocument();
+		expect(checkbox).toHaveLength(3);
+
+		userEvent.click(checkbox[0]);
+
+		expect(checkbox[0]).toBeChecked()
+		expect(checkbox[1]).not.toBeChecked()
+		expect(checkbox[2]).not.toBeChecked()
 	});
 });
