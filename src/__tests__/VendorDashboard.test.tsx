3f8dea7ea892e75fc06ead36e8558e08dec30c04
Eric Berglund <ericberglund117@gmail.com>
2021-01-11 19:12:10 -0700

    Create VendorDashboard.test file

---
+++ src/__tests__/VendorDashboard.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event'
+
+import VendorDashboard from '../components/VendorDashboard/VendorDashboard';
+
+describe('VendorDashboard', () => {
+	it('should render the vendor dashboard', () => {
+
+	})
+})
