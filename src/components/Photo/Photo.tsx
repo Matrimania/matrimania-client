cabc78979bfbbb863efc91376572f6eeebfd6ad4
Kristi Miller <kristi.ann.miller@gmail.com>
2021-01-13 08:24:09 -0700

    Refactor to accomodate api data

--- src/components/Photo/Photo.tsx
+++ src/components/Photo/Photo.tsx
@@ -4,7 +4,7 @@ import './Photo.css';
 type Props = {
   id: number;
   photoNumber: number;
-  guests: any;
+  guests: string[];
   description: string;
 }
 const Photo: React.FC<Props> = ({
@@ -13,12 +13,14 @@ const Photo: React.FC<Props> = ({
   guests,
   description,
 }) => {
+  console.log("");
+
   return (
     < article className="photoCard">
-      <h4 className="photoHeader">PHOTO {photoNumber}</h4>
+      <h4 className="photoHeader">{`PHOTO ${photoNumber}`}</h4>
       <p>
         {guests.length > 0 && guests.reduce((names:string, guest: any) => {
-        names += ` ${guest.name} •`
+        names += ` ${guest} •`
         return names
         }, '•')
       }
