b69506d8b9cbda5c945d34a2ea70371b35109b0c
Kristi Miller <kristi.ann.miller@gmail.com>
2021-01-14 23:02:10 -0700

    Add responsive design to PhotoShootView component

--- src/components/PhotoShootView/PhotoShootView.tsx
+++ src/components/PhotoShootView/PhotoShootView.tsx
@@ -68,13 +68,14 @@ const PhotoShootView: React.FC<PhotoShootData> = ({
 			})
 			if(participants.length > 0) {
 				return (
-					<StyledCard contents="photoShoot">
+					<StyledCard contents="other">
 					<article className="photoShootCard">
 					<Photo
 					id={currentPhoto.id}
 					photoNumber={currentPhoto.number}
 					guests={participants}
 					description={currentPhoto.description}
+					location={'shoot'}
 					/>
 					</article>
 					</StyledCard>
@@ -91,20 +92,18 @@ const PhotoShootView: React.FC<PhotoShootData> = ({
 		{view === "notify" &&
 			<>
 			<article className="instructionWrap">
-				<h1 className="weddingTitle" style={{fontSize: '3vw', paddingBottom: '0%'}}>Ready, Set, Click.</h1>
-				<h2 className="weddingDate" style={{fontSize: '1.5vw', padding: '2% 10%'}}>Clicking the <span style={{color: '#6f896d', fontWeight: 'bold'}}>{` 'Notify Guests'`}</span> button below will:</h2>
-				<h2 className="weddingDate">1. Text message all photo participants.<br></br><br></br>2. Display the details for each photo.</h2>
+				<h1 id="shootTitle" className="weddingTitle">Ready, Set, Click.</h1>
+				<h2 id="shootBlurb" className="weddingDate">Clicking the <span style={{color: '#6f896d', fontWeight: 'bold'}}>{` 'Notify Guests'`}</span> button below will:</h2>
+				<h2 id="shootDirections" className="weddingDate">1. Text message all photo participants.<br></br><br></br>2. Display the details for each photo.</h2>
 			</article>
 			<section className="instructions">Sample Message:<br></br><br></br>
-				<p className="textBody">{`Hello [Guest's Name], Thank you for attending the ${name} wedding.`}
-					<br></br>{`The lovely couple would like you to meet them for photos ${time !== "" ? time : '[when]'} ${location !== "" ? location : '[where]'}.`}
-					<br></br>{`You will be participating in photo numbers...`}
+				<p className="textBody">{`Hello [Guest's Name], Thank you for attending the ${name} wedding. The lovely couple would like you to meet them for photos ${time !== "" ? time : '[when]'} ${location !== "" ? location : '[where]'}. You will be participating in photo numbers...`}
 				</p>
 			</section>
-			<form className="weddingFormWrapper">
+			<form className="shootFormWrapper">
 				<label htmlFor="when">When should guests meet? (ex: "after the ceremony")</label>
 				<input
-				style={{width: '25%', margin: '0% 0% 1% 0%'}}
+				id="shootInput"
 				type='text'
 				placeholder='When?'
 				name='when'
@@ -113,7 +112,7 @@ const PhotoShootView: React.FC<PhotoShootData> = ({
 				/>
 				<label htmlFor="where">Where should guests meet? (ex: "at the ceremony site")</label>
 				<input
-				style={{width: '25%', margin: '0% 0% 1% 0%'}}
+				id="shootInput"
 				type='text'
 				placeholder='Where?'
 				name='where'
@@ -129,7 +128,7 @@ const PhotoShootView: React.FC<PhotoShootData> = ({
 			{view === "shoot" &&
 				<>
 				<article className="carouselWrap">
-					<h1 className="weddingTitle" style={{fontSize: '3vw', paddingBottom: '0%'}}>Ready, Set, Click.</h1>
+					<h1 id="shootTitle2" className="weddingTitle">Ready, Set, Click.</h1>
 						{displayCarousel()}
 					<section className="buttonWrapper">
 					{(carousel > 0 && carousel <= photoList.length) &&
