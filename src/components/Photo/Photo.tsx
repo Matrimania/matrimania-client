1799f9efaa3022ab73c7f617ffb2201aeb85c6b2
Kristi Miller <kristi.ann.miller@gmail.com>
2021-01-14 23:01:14 -0700

    Add responsive design to Photo component

--- src/components/Photo/Photo.tsx
+++ src/components/Photo/Photo.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import './Photo.css';
+import { PhotoCard } from '../App/styledComponents.styles';
+
 
 type Props = {
   id: number;
   photoNumber: number;
   guests: string[];
   description: string;
+  location: string;
 }
 const Photo: React.FC<Props> = ({
   id,
   photoNumber,
   guests,
   description,
+  location
 }) => {
 
   return (
-    < article className="photoCard">
+    <PhotoCard contents={location}>
       <h4 className="photoHeader">{`PHOTO ${photoNumber}`}</h4>
-      <p>
+      <p className="guestData">
         {guests.length > 0 && guests.reduce((names:string, guest: any) => {
         names += ` ${guest} •`
         return names
@@ -26,7 +30,7 @@ const Photo: React.FC<Props> = ({
       </p>
 
       <p className="photoDescription">Description: {description}</p>
-    </article>
+    </PhotoCard>
   )
 }
 
