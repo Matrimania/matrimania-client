71df10d9718189e91a4178dbfc3c351a2a1cca1f
Hanna Kim <hannakim91@gmail.com>
2021-01-29 11:05:59 -0700

    Add imports to App test file

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 
 import App from '../components/App/App';
+import { getWeddings } from '../apiCalls';
+
+jest.mock('../apiCalls.tsx');
 
 describe('App', () => {
   it('renders App elements', () => {
