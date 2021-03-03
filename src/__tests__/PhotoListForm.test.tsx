a8ca36d9f9977b033b8557dbcc0f4e4e454d0bea
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-03 11:12:41 -0700

    Add happy and sad path tests for submitting a photo

--- src/__tests__/PhotoListForm.test.tsx
+++ src/__tests__/PhotoListForm.test.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event'
-
 import PhotoListForm from '../components/PhotoListForm/PhotoListForm';
 
 describe('PhotoListForm', () => {
   it('renders PhotoList Form with guests', () => {
 		const mockChangeView = jest.fn()
-	
+
     render(
       <MemoryRouter>
         <PhotoListForm
@@ -19,8 +18,6 @@ describe('PhotoListForm', () => {
 							{id: 2, name: 'Bohn', phone: 1111151234, wedding: 1},
 							{id: 3, name: 'Bamy', phone: 2222221234, wedding: 1}
 					]}
-					updateGuests
-					updatePhotos
 					changeView={mockChangeView}
 					photoList={[
 							{id: 1, photoNumber: 1, guest: ['Bacob, Bohn, Bamy'], desciption: 'Rowdy'},
@@ -28,9 +25,9 @@ describe('PhotoListForm', () => {
 							{id: 3, photoNumber: 3, guest: ['Bacob, Bohn, Bamy'], desciption: 'Fun'}
 					]}
       />
-      </MemoryRouter> 
+      </MemoryRouter>
     );
-	
+
 		expect(screen.getByRole('heading', {name: /let's build your photo list/i})).toBeInTheDocument();
 		expect(screen.getByRole('heading', {name: `To add a photo: 1. Add a description 2. Pick guests to include in the photo 3. Click Submit button Tip: Don't forget to include yourselves!`})).toBeInTheDocument();
 		expect(screen.getByPlaceholderText('Description (optional)')).toBeInTheDocument();
@@ -40,24 +37,22 @@ describe('PhotoListForm', () => {
 		expect(screen.getByText("Bamy")).toBeInTheDocument();
 		expect(screen.getAllByRole('checkbox')).toHaveLength(3);
 	});
-	
+
 	it('renders PhotoList Form if there are no guests guests', () => {
 		const mockChangeView = jest.fn()
-	
+
     render(
       <MemoryRouter>
         <PhotoListForm
         loading={false}
 				weddingId={1}
 				guests={[]}
-				updateGuests
-				updatePhotos
 				changeView={mockChangeView}
 				photoList={[]}
       />
-      </MemoryRouter> 
+      </MemoryRouter>
     );
-	
+
 		expect(screen.getByRole('heading', {name: /let's build your photo list/i})).toBeInTheDocument();
 		expect(screen.getByRole('heading', {name: `To add a photo: 1. Add a description 2. Pick guests to include in the photo 3. Click Submit button Tip: Don't forget to include yourselves!`})).toBeInTheDocument();
 		expect(screen.getByPlaceholderText('Description (optional)')).toBeInTheDocument();
@@ -66,7 +61,6 @@ describe('PhotoListForm', () => {
 
 	it('should allow the user to check guests', () => {
 		const mockChangeView = jest.fn()
-		const mockGetGuestNames = jest.fn().mockImplementation
     render(
       <MemoryRouter>
         <PhotoListForm
@@ -77,8 +71,6 @@ describe('PhotoListForm', () => {
 						{id: 2, name: 'Bohn', phone: 1111151234, wedding: 1},
 						{id: 3, name: 'Bamy', phone: 2222221234, wedding: 1}
 					]}
-					updateGuests
-					updatePhotos
 					changeView={mockChangeView}
 					photoList={[
 						{id: 1, photoNumber: 1, guest: ['Bacob, Bohn, Bamy'], desciption: 'Rowdy'},
@@ -86,9 +78,9 @@ describe('PhotoListForm', () => {
 						{id: 3, photoNumber: 3, guest: ['Bacob, Bohn, Bamy'], desciption: 'Fun'}
 				]}
       />
-      </MemoryRouter> 
+      </MemoryRouter>
     );
-				
+
 		const checkbox = screen.getAllByRole('checkbox')
 
 		expect(screen.getByRole('heading', {name: /let's build your photo list/i})).toBeInTheDocument();
@@ -106,6 +98,88 @@ describe('PhotoListForm', () => {
 		expect(checkbox[1]).not.toBeChecked()
 		expect(checkbox[2]).not.toBeChecked()
 	});
-	//user submits form? integration? 
-	// api call mockimplementation or toHaveReturned(data)
+  it('should submit a photo', () => {
+		const mockChangeView = jest.fn()
+    const mockUpdatePhotoList = jest.fn()
+    render(
+      <MemoryRouter>
+        <PhotoListForm
+					loading={false}
+					weddingId={1}
+					guests={[
+						{id: 1, name: 'Bacob', phone: 5555551234, wedding: 1 },
+						{id: 2, name: 'Bohn', phone: 1111151234, wedding: 1},
+						{id: 3, name: 'Bamy', phone: 2222221234, wedding: 1}
+					]}
+					changeView={mockChangeView}
+					photoList={[
+						{id: 1, photoNumber: 1, guest: ['Bacob, Bohn, Bamy'], desciption: 'Rowdy'},
+						{id: 2, photoNumber: 2, guest: ['Bacob, Bohn, Bamy'], desciption: 'Old'},
+						{id: 3, photoNumber: 3, guest: ['Bacob, Bohn, Bamy'], desciption: 'Fun'}
+				  ]}
+          updatePhotoList={mockUpdatePhotoList}
+
+      />
+      </MemoryRouter>
+    );
+
+		const checkbox = screen.getAllByRole('checkbox')
+
+		expect(screen.getByRole('heading', {name: /let's build your photo list/i})).toBeInTheDocument();
+		expect(screen.getByRole('heading', {name: `To add a photo: 1. Add a description 2. Pick guests to include in the photo 3. Click Submit button Tip: Don't forget to include yourselves!`})).toBeInTheDocument();
+		expect(screen.getByPlaceholderText('Description (optional)')).toBeInTheDocument();
+		expect(screen.getByText('Submit Photo')).toBeInTheDocument();
+		expect(screen.getByText("Bacob")).toBeInTheDocument();
+		expect(screen.getByText("Bohn")).toBeInTheDocument();
+		expect(screen.getByText("Bamy")).toBeInTheDocument();
+		expect(checkbox).toHaveLength(3);
+
+		userEvent.click(checkbox[0]);
+    userEvent.type(screen.getByPlaceholderText('Description (optional)'), 'Just Bacob')
+    userEvent.click(screen.getByText('Submit Photo'))
+
+    expect(mockUpdatePhotoList).toHaveBeenCalled()
+
+	});
+  it('should display an error if a user submits a photo with no guests selected', () => {
+    const mockChangeView = jest.fn()
+    const mockUpdatePhotoList = jest.fn()
+    render(
+      <MemoryRouter>
+        <PhotoListForm
+          loading={false}
+          weddingId={1}
+          guests={[
+            {id: 1, name: 'Bacob', phone: 5555551234, wedding: 1 },
+            {id: 2, name: 'Bohn', phone: 1111151234, wedding: 1},
+            {id: 3, name: 'Bamy', phone: 2222221234, wedding: 1}
+          ]}
+          changeView={mockChangeView}
+          photoList={[
+            {id: 1, photoNumber: 1, guest: ['Bacob, Bohn, Bamy'], desciption: 'Rowdy'},
+            {id: 2, photoNumber: 2, guest: ['Bacob, Bohn, Bamy'], desciption: 'Old'},
+            {id: 3, photoNumber: 3, guest: ['Bacob, Bohn, Bamy'], desciption: 'Fun'}
+          ]}
+          updatePhotoList={mockUpdatePhotoList}
+
+      />
+      </MemoryRouter>
+    );
+
+    const checkbox = screen.getAllByRole('checkbox')
+
+    expect(screen.getByRole('heading', {name: /let's build your photo list/i})).toBeInTheDocument();
+    expect(screen.getByRole('heading', {name: `To add a photo: 1. Add a description 2. Pick guests to include in the photo 3. Click Submit button Tip: Don't forget to include yourselves!`})).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Description (optional)')).toBeInTheDocument();
+    expect(screen.getByText('Submit Photo')).toBeInTheDocument();
+    expect(screen.getByText("Bacob")).toBeInTheDocument();
+    expect(screen.getByText("Bohn")).toBeInTheDocument();
+    expect(screen.getByText("Bamy")).toBeInTheDocument();
+    expect(checkbox).toHaveLength(3);
+
+    userEvent.type(screen.getByPlaceholderText('Description (optional)'), 'Just Bacob')
+    userEvent.click(screen.getByText('Submit Photo'))
+
+    expect(screen.getByText('Please select at least one guest for the photo')).toBeInTheDocument()
+  });
 });
