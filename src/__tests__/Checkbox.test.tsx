3b6d0388ca7cb646db1cb1cd126028c3294e6721
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-03 20:30:59 -0700

    Clean up spacing

--- src/__tests__/Checkbox.test.tsx
+++ src/__tests__/Checkbox.test.tsx
@@ -1,40 +1,41 @@
 import React, { useState } from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
-
+import userEvent from '@testing-library/user-event';
 import Checkbox from '../components/Checkbox/Checkbox';
 
 describe('Checkbox', () => {
-	it('should display checkboxes next to a guest', () => {
+
+	it('should display checkbox next to a guest', () => {
+		const mockToggleCheckMark = jest.fn();
+
 		render(
 			<Checkbox
 				name={'Mayer Hawthorne'}
 				isChecked={false}
-				toggleCheckMark={jest.fn()}
+				toggleCheckMark={mockToggleCheckMark}
 			/>
 		);
 
-		const checkbox = screen.getByRole('checkbox')
+		const checkbox = screen.getByRole('checkbox');
 		expect(checkbox).toBeInTheDocument();
 		expect(screen.getByText('Mayer Hawthorne')).toBeInTheDocument();
 	});
 
 	it('should put a check next to a guest when a user clicks a checkbox', () => {
-		const mockToggleCheckMark = jest.fn()
-		const isChecked = false
+		const mockToggleCheckMark = jest.fn();
+
 		render(
 			<Checkbox
 				name={'Mayer Hawthorne'}
-				isChecked={isChecked}
+				isChecked={false}
 				toggleCheckMark={mockToggleCheckMark}
 			/>
 		);
 
-		const checkbox = screen.getByRole('checkbox')
+		const checkbox = screen.getByRole('checkbox');
 		expect(checkbox).toBeInTheDocument();
 		expect(screen.getByText('Mayer Hawthorne')).toBeInTheDocument();
-		userEvent.click(checkbox)
+		userEvent.click(checkbox);
 		expect(mockToggleCheckMark).toHaveBeenCalled();
-		
 	});
 });
