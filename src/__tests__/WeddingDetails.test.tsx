81e027cb8515386ea2c2f645bd316fb79b05a5cc
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-03 18:42:46 -0700

    Add test for PhotoListForm render

--- src/__tests__/WeddingDetails.test.tsx
+++ src/__tests__/WeddingDetails.test.tsx
@@ -290,6 +290,54 @@ describe('WeddingDetails', () => {
     expect(guestListTitle).toBeInTheDocument();
     expect(guestListInstructions).toBeInTheDocument();
   });
+  it('should render the PhotoListForm', () => {
+    const mockDeleteWedding = jest.fn()
+    const mockLoadWedding = jest.fn()
+    const mockUpdateGuests = jest.fn()
+    const mockUpdatePhotos = jest.fn()
 
-  // should render the PhotoListForm
+    render(
+      <MemoryRouter>
+        <WeddingDetails
+          weddingId={1}
+          currentWeddingData={
+            {id: 1, name: "Henderson", email: "email@aol.com", date: "01/02/2022", image: "image.coolurl.com"}
+          }
+          guests={[
+            {id: 1, name: "Bob", phoneNumber: "303-222-8888", wedding: 1}
+          ]}
+          photos={[
+            {id: 1, number: 1, description: "just bob", guest: [1], weddingId: 1}
+          ]}
+          deleteSingleWedding={mockDeleteWedding}
+          loadWeddingData={mockLoadWedding}
+          error={{photoError: '', guestError: '', weddingError: ''}}
+          updateGuests={mockUpdateGuests}
+          updatePhotoList={mockUpdatePhotos}
+        />
+    </MemoryRouter>
+    );
+    expect(screen.getByText('Henderson Wedding')).toBeInTheDocument();
+    expect(screen.getByText('01/02/2022')).toBeInTheDocument();
+    expect(screen.getByText('Email: email@aol.com')).toBeInTheDocument();
+    expect(screen.getByText('Edit Photo Details')).toBeInTheDocument()
+
+    userEvent.click(screen.getByText('Edit Photo Details'))
+
+    const guestListTitle = screen.getByText(`Let's start with your guest list`);
+    const guestListInstructions = screen.getByRole('heading', {  name: `For each person included in your family photos, please include: 1. Their first and last name 2. A mobile phone number that accepts text messages Don't forget yourselves!`})
+
+    expect(guestListTitle).toBeInTheDocument();
+    expect(guestListInstructions).toBeInTheDocument();
+    expect(screen.getByText('Photos >')).toBeInTheDocument()
+
+    userEvent.click(screen.getByText('Photos >'))
+
+    expect(screen.getByRole('heading', {name: /let's build your photo list/i})).toBeInTheDocument();
+    expect(screen.getByRole('heading', {name: `To add a photo: 1. Add a description 2. Pick guests to include in the photo 3. Click Submit button Tip: Don't forget to include yourselves!`})).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Description (optional)')).toBeInTheDocument();
+
+  });
+
+  //
 });
