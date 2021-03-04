66c43486fd9c842a9fdd35f4111e3c85356c453c
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-03 17:53:47 -0700

    Add wedding data to WeddingDetails render

--- src/__tests__/WeddingDetails.test.tsx
+++ src/__tests__/WeddingDetails.test.tsx
@@ -6,46 +6,72 @@ import WeddingDetails from '../components/WeddingDetails/WeddingDetails';
 
 describe('WeddingDetails', () => {
   it('renders default WeddingDetails elements', () => {
-    const detailsView = true;
+    const mockDeleteWedding = jest.fn()
+    const mockLoadWedding = jest.fn()
+    const mockUpdateGuests = jest.fn()
+    const mockUpdatePhotos = jest.fn()
 
     render(
       <MemoryRouter>
         <WeddingDetails
           weddingId={1}
+          currentWeddingData={
+            {id: 1, name: "Henderson", email: "email@aol.com", date: "01/02/2022", image: "image.coolurl.com"}
+          }
+          guests={[
+            {id: 1, name: "Bob", phoneNumber: "303-222-8888", wedding: 1}
+          ]}
+          photos={[
+          ]}
+          deleteSingleWedding={mockDeleteWedding}
+          loadWeddingData={mockLoadWedding}
+          error={{photoError: '', guestError: '', weddingError: ''}}
+          updateGuests={mockUpdateGuests}
+          updatePhotoList={mockUpdatePhotos}
         />
     </MemoryRouter>
     );
-    
-    expect(detailsView).toEqual(true)
-    expect(screen.getByText('Wedding')).toBeInTheDocument();
-    expect(screen.getByText('Invalid Date')).toBeInTheDocument()
-    expect(screen.getByTestId('emailSection')).toBeInTheDocument()
+
+    expect(screen.getByText('Henderson Wedding')).toBeInTheDocument();
+    expect(screen.getByText('01/02/2022')).toBeInTheDocument()
+    expect(screen.getByText('Email: email@aol.com')).toBeInTheDocument()
     expect(screen.getByTestId('status')).toBeInTheDocument()
-    expect(screen.getByAltText("detailImage")).toBeInTheDocument()
+    expect(screen.getByAltText('detailImage')).toBeInTheDocument()
   });
 
-  it('should render requestPhotoList view if no photos exist for this wedding', () => {
-    const detailsView = false; 
-    const requestPhotoView = true;
+  it('should render a request button if no photos exist for this wedding', () => {
+    const mockDeleteWedding = jest.fn()
+    const mockLoadWedding = jest.fn()
+    const mockUpdateGuests = jest.fn()
+    const mockUpdatePhotos = jest.fn()
 
     render(
       <MemoryRouter>
         <WeddingDetails
           weddingId={1}
+          currentWeddingData={
+            {id: 1, name: "Henderson", email: "email@aol.com", date: "01/02/2022", image: "image.coolurl.com"}
+          }
+          guests={[
+            {id: 1, name: "Bob", phoneNumber: "303-222-8888", wedding: 1}
+          ]}
+          photos={[
+          ]}
+          deleteSingleWedding={mockDeleteWedding}
+          loadWeddingData={mockLoadWedding}
+          error={{photoError: '', guestError: '', weddingError: ''}}
+          updateGuests={mockUpdateGuests}
+          updatePhotoList={mockUpdatePhotos}
         />
     </MemoryRouter>
     );
-    
-    expect(detailsView).toEqual(false)
-    expect(requestPhotoView).toEqual(true)
-    expect(screen.getByText('Wedding')).toBeInTheDocument();
-    expect(screen.getByText('Invalid Date')).toBeInTheDocument()
-    expect(screen.getByTestId('emailSection')).toBeInTheDocument()
-    expect(screen.getByTestId('status')).toBeInTheDocument()
-    expect(screen.getByAltText("detailImage")).toBeInTheDocument()
-    expect(screen.getByText("Request Photo List")).toBeInTheDocument()
-    expect(screen.getByText("Add Photo List")).toBeInTheDocument()
-    expect(screen.getByAltText("detailImage")).toBeInTheDocument()
-  });
 
+    expect(screen.getByText('Henderson Wedding')).toBeInTheDocument();
+    expect(screen.getByText('01/02/2022')).toBeInTheDocument()
+    expect(screen.getByText('Email: email@aol.com')).toBeInTheDocument()
+    expect(screen.getByTestId('status')).toBeInTheDocument()
+    expect(screen.getByAltText('detailImage')).toBeInTheDocument()
+    expect(screen.getByText('Request Photo List')).toBeInTheDocument()
+    expect(screen.getByText('Add Photo List')).toBeInTheDocument()
+  });
 });
