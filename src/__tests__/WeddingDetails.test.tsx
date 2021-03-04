c031d9694f12d2bfcb1e93378282ba9dd0949933
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-03 21:39:20 -0700

    Add delete wedding test

--- src/__tests__/WeddingDetails.test.tsx
+++ src/__tests__/WeddingDetails.test.tsx
@@ -3,12 +3,18 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { individualWedding } from '../weddingData'
 import WeddingDetails from '../components/WeddingDetails/WeddingDetails';
+import App from '../components/App/App';
 import dayjs from 'dayjs';
 import userEvent from '@testing-library/user-event';
+import { getWeddings, postAWedding, getSingleWeddingPhotos, getSingleWeddingGuests, deleteWedding, postAGuest, postAPhoto } from '../apiCalls';
+
+
+jest.mock('../apiCalls.tsx');
+
 
 
 describe('WeddingDetails', () => {
-  
+
   it('renders default WeddingDetails elements', () => {
     const mockDeleteWedding = jest.fn();
     const mockLoadWedding = jest.fn();
@@ -117,7 +123,7 @@ describe('WeddingDetails', () => {
     expect(screen.getByText('Edit Photo Details')).toBeInTheDocument();
   });
 
-  it('should display an "Start Photo Session" button if the wedding takes place on the current day', () => {
+  it('should display a "Start Photo Session" button if the wedding takes place on the current day', () => {
     const mockDeleteWedding = jest.fn();
     const mockLoadWedding = jest.fn();
     const mockUpdateGuests = jest.fn();
@@ -222,6 +228,43 @@ describe('WeddingDetails', () => {
     expect(screen.getByText('01/02/2022')).toBeInTheDocument();
     expect(screen.getByText('Email: email@aol.com')).toBeInTheDocument();
     expect(screen.queryByText('Wedding Photos')).not.toBeInTheDocument();
+  });
+
+  it('should delete a wedding', async () => {
+    getWeddings.mockResolvedValue([
+      {
+        "id": 31,
+        "name": "Bueller",
+        "email": "saveferris@netscape.com",
+        "date": "01/28/2021",
+        "image": "https://imagelink.com"
+      }
+    ])
+    getSingleWeddingPhotos.mockResolvedValue([])
+    getSingleWeddingGuests.mockResolvedValue([])
+    deleteWedding.mockResolvedValue({
+      "id": 31,
+      "name": "Bueller",
+      "email": "saveferris@netscape.com",
+      "date": "01/28/2021",
+      "image": "https://imagelink.com"
+    });
+
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    const wedding2Link = await waitFor(() => screen.getByTestId("Bueller-link"));
+    expect(wedding2Link).toBeInTheDocument();
+    userEvent.click(wedding2Link);
+
+    const deleteButton = await waitFor(() => screen.getByText("Delete Wedding"));
+    userEvent.click(deleteButton);
+
+    const noWeddingsImage = await waitFor(() => screen.getByAltText("No weddings in storage"));
+    expect(noWeddingsImage).toBeInTheDocument();
   });
 
   it('should fire a function to delete the current wedding', () => {
