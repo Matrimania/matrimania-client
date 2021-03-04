079480a95c9ad9fbb6f3251da2f5d22b527ba783
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-03 20:08:59 -0700

    Adds test assuring that wedding can be added without an image

--- src/__tests__/AddWeddingForm.test.tsx
+++ src/__tests__/AddWeddingForm.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
-
+import { postAWedding } from '../apiCalls.tsx';
 import AddWeddingForm from '../components/AddWeddingForm/AddWeddingForm';
+jest.mock('../apiCalls.tsx');
+
 
 describe('Add Wedding Form', () => {
   it('renders all Add Wedding Form elements', () => {
@@ -47,4 +49,36 @@ describe('Add Wedding Form', () => {
     const errorMessage = screen.getByText("Please makes sure to include your name, email, and wedding date");
     expect(errorMessage).toBeInTheDocument();
   });
-});
+
+  it('allows form submission without image field filled', async () => {
+    const mockAddNewWedding = jest.fn()
+    postAWedding.mockResolvedValue({
+      "id": 33,
+      "name": "Banks",
+      "email": "philNviv@belair.com",
+      "date": "02/22/2022",
+      "image": "https://image2.org"
+    })
+
+    render(
+      <MemoryRouter>
+        <AddWeddingForm
+          addNewWedding={mockAddNewWedding} />
+      </MemoryRouter>
+    );
+
+    const nameInput = screen.getByPlaceholderText("Last Name");
+    const emailInput = screen.getByPlaceholderText("Email Address");
+    const dateInput = screen.getByPlaceholderText("Wedding Date");
+    const imageInput = screen.getByPlaceholderText("Image Link (optional)");
+    const submitButton = screen.getByText("Submit Wedding");
+
+    userEvent.type(nameInput, 'Banks')
+    userEvent.type(emailInput, 'philNviv@belair.com')
+    userEvent.type(dateInput, '2022-02-22')
+    userEvent.click(submitButton)
+
+    await waitFor(() => expect(mockAddNewWedding).toHaveBeenCalled())
+    expect(postAWedding).toHaveBeenCalled()
+
+  });
