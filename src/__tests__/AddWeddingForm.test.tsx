3faab04a5562d0d3a1a66c6e063b0dcdc8b1ccb0
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-03 20:11:36 -0700

    Clean up spacing

--- src/__tests__/AddWeddingForm.test.tsx
+++ src/__tests__/AddWeddingForm.test.tsx
@@ -9,7 +9,7 @@ jest.mock('../apiCalls.tsx');
 
 describe('Add Wedding Form', () => {
   it('renders all Add Wedding Form elements', () => {
-    const mockAddNewWedding = jest.fn()
+    const mockAddNewWedding = jest.fn();
 
     render(
       <MemoryRouter>
@@ -34,7 +34,7 @@ describe('Add Wedding Form', () => {
   });
 
   it('prevents form submission without required fields filled', () => {
-    const mockAddNewWedding = jest.fn()
+    const mockAddNewWedding = jest.fn();
 
     render(
       <MemoryRouter>
@@ -44,21 +44,21 @@ describe('Add Wedding Form', () => {
     );
 
     const submitButton = screen.getByText("Submit Wedding");
-    userEvent.click(submitButton)
+    userEvent.click(submitButton);
 
     const errorMessage = screen.getByText("Please makes sure to include your name, email, and wedding date");
     expect(errorMessage).toBeInTheDocument();
   });
 
-  it('allows form submission without image field filled', async () => {
-    const mockAddNewWedding = jest.fn()
+  it('should add a new wedding', async () => {
+    const mockAddNewWedding = jest.fn();
     postAWedding.mockResolvedValue({
       "id": 33,
       "name": "Banks",
       "email": "philNviv@belair.com",
       "date": "02/22/2022",
       "image": "https://image2.org"
-    })
+    });
 
     render(
       <MemoryRouter>
@@ -73,31 +73,30 @@ describe('Add Wedding Form', () => {
     const imageInput = screen.getByPlaceholderText("Image Link (optional)");
     const submitButton = screen.getByText("Submit Wedding");
 
-    userEvent.type(nameInput, 'Banks')
-    userEvent.type(emailInput, 'philNviv@belair.com')
-    userEvent.type(dateInput, '2022-02-22')
-    userEvent.click(submitButton)
+    userEvent.type(nameInput, 'Banks');
+    userEvent.type(emailInput, 'philNviv@belair.com');
+    userEvent.type(dateInput, '2022-02-22');
+    userEvent.type(imageInput, 'www.image.com/image.jpg');
+    userEvent.click(submitButton);
 
-    await waitFor(() => expect(mockAddNewWedding).toHaveBeenCalled())
-    expect(postAWedding).toHaveBeenCalled()
-
+    await waitFor(() => expect(mockAddNewWedding).toHaveBeenCalled());
+    expect(postAWedding).toHaveBeenCalled();
   });
 
-
-  it('should add a new wedding', async () => {
-    const mockAddNewWedding = jest.fn()
+  it('allows form submission without image field filled', async () => {
+    const mockAddNewWedding = jest.fn();
     postAWedding.mockResolvedValue({
       "id": 33,
       "name": "Banks",
       "email": "philNviv@belair.com",
       "date": "02/22/2022",
       "image": "https://image2.org"
-    })
+    });
 
     render(
       <MemoryRouter>
-        <AddWeddingForm
-          addNewWedding={mockAddNewWedding} />
+      <AddWeddingForm
+      addNewWedding={mockAddNewWedding} />
       </MemoryRouter>
     );
 
@@ -107,13 +106,12 @@ describe('Add Wedding Form', () => {
     const imageInput = screen.getByPlaceholderText("Image Link (optional)");
     const submitButton = screen.getByText("Submit Wedding");
 
-    userEvent.type(nameInput, 'Banks')
-    userEvent.type(emailInput, 'philNviv@belair.com')
-    userEvent.type(dateInput, '2022-02-22')
-    userEvent.type(imageInput, 'www.image.com/image.jpg')
-    userEvent.click(submitButton)
+    userEvent.type(nameInput, 'Banks');
+    userEvent.type(emailInput, 'philNviv@belair.com');
+    userEvent.type(dateInput, '2022-02-22');
+    userEvent.click(submitButton);
 
-    await waitFor(() => expect(mockAddNewWedding).toHaveBeenCalled())
-    expect(postAWedding).toHaveBeenCalled()
+    await waitFor(() => expect(mockAddNewWedding).toHaveBeenCalled());
+    expect(postAWedding).toHaveBeenCalled();
   });
 });
